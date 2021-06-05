eed92302795272bcf99bada14afbd6a3dcc55db6
masaki <robomasaki2@gmail.com>
2021-06-05 14:28:22 +0900

    feat: changed api response type

--- app/src/main/java/jp/aoyama/mki/thermometer/infrastructure/bluetooth/api/BluetoothApiService.kt
+++ app/src/main/java/jp/aoyama/mki/thermometer/infrastructure/bluetooth/api/BluetoothApiService.kt
@@ -3,10 +3,6 @@ package jp.aoyama.mki.thermometer.infrastructure.bluetooth.api
 import retrofit2.Call
 import retrofit2.http.GET
 
-data class BluetoothScanResponse(
-    val results: List<BluetoothDevice>
-)
-
 data class BluetoothDevice(
     val address: String = "",
     val found: Boolean = false
